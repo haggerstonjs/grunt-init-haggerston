e527ae60bdec510637a72f76ac34958b85fd8d7d
Kelvin Luck <git@kelvinluck.com>
2013-04-08 22:52:41 +0200

    Basic grunt-init task which allows you to generate a bare bone grunt-haggerston project quickly and easily

---
+++ root/Gruntfile.js
@@ -0,0 +1,63 @@
+/*
+ * {%= name %}
+ * {%= homepage %}
+ *
+ * Copyright (c) {%= grunt.template.today('yyyy') %} {%= author_name %}
+ * Licensed under the {%= licenses.join(', ') %} license{%= licenses.length === 1 ? '' : 's' %}.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    clean: ['{%= out_path %}/'],
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: '{%= assets_path %}/',
+            src: ['**'],
+            dest: '{%= out_path %}/'
+          }
+        ]
+      }
+    },
+    haggerston: {
+
+    },
+    watch: {
+      content: {
+        files: [
+          '{%= src_path %}/content/**/*.json',
+          '{%= src_path %}/content/**/*.md',
+          '{%= src_path %}/templates/**/*.html'
+        ],
+        tasks: 'build'
+      }
+    },
+    connect: {
+      site: {
+        options: {
+          base: '{%= out_path %}/'
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-haggerston');
+
+  grunt.registerTask('build', ['clean', 'copy', 'stylus', 'haggerston']);
+  grunt.registerTask('serve', ['build', 'connect', 'watch']);
+
+  grunt.registerTask('default', ['build']);
+
+};
---
+++ template.js
@@ -0,0 +1,89 @@
+/*
+ * grunt-init-haggerston
+ * https://github.com/haggerstonjs/grunt-init-haggerston
+ *
+ * Copyright (c) 2013 Kelvin Luck, contributors
+ * Licensed under the MIT license.
+ *
+ * Based on grunt-init-gruntplugin by "Cowboy" Ben Alman
+ * https://github.com/gruntjs/grunt-init-gruntplugin
+ */
+
+'use strict';
+
+exports.description = 'Create a HaggerstonJS website.';
+
+exports.notes = 'For more information about HaggerstonJS, ' +
+  'please see the docs at http://github.com/haggerstonjs/website';
+
+exports.after = 'You should now install project dependencies with _npm ' +
+  'install_. After that, you may execute project tasks with _grunt_. For ' +
+  'more information about installing and configuring Grunt, please see ' +
+  'the Getting Started guide:' +
+  '\n\n' +
+  'http://gruntjs.com/getting-started';
+
+exports.warnOn = '*';
+
+exports.template = function(grunt, init, done) {
+
+  init.process({type: 'grunt'}, [
+    // Prompt for these values.
+    init.prompt('name', 'The name of your website'),
+    init.prompt('description', 'A description of your website.'),
+    {
+      name: 'src_path',
+      default: 'src'
+    },
+    {
+      name: 'out_path',
+      default: 'out'
+    },
+    {
+      name: 'assets_path',
+      default: 'assets'
+    },
+    init.prompt('version'),
+    init.prompt('homepage'),
+    init.prompt('licenses'),
+    init.prompt('author_name'),
+    init.prompt('author_email'),
+    init.prompt('author_url'),
+    init.prompt('grunt_version'),
+    init.prompt('node_version', grunt.package.engines.node)
+  ], function(err, props) {
+    // Set a few grunt-plugin-specific properties.
+    props.main = 'Gruntfile.js';
+    props.keywords = ['haggerstonjs'];
+    props.devDependencies = {
+    };
+    props.peerDependencies = {
+      'grunt': props.grunt_version
+    };
+    props.dependencies = {
+      'grunt-haggerston': 'git@github.com:haggerstonjs/grunt-haggerston.git',
+      'grunt-contrib-clean': '~0.4.0',
+      'grunt-contrib-copy': '~0.4.0',
+      'grunt-contrib-watch': '~0.2.0',
+      'grunt-contrib-connect': '~0.1.2'
+    };
+
+    // Files to copy (and process).
+    var files = init.filesToCopy(props);
+
+    // Add properly-named license files.
+    init.addLicenseFiles(files, props.licenses);
+
+    console.log(props);
+
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props);
+
+    // Generate package.json file.
+    init.writePackageJSON('package.json', props);
+
+    // All done!
+    done();
+  });
+
+};
