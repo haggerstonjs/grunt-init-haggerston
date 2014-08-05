12fd40dd7a64d2ac06bff4a1564cbbea0fc8b31c
Kelvin Luck <git@kelvinluck.com>
2014-08-05 10:47:48 +0100

    Updated task to more up to date dependencies and more complete project structure

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -8,55 +8,18 @@
 
 'use strict';
 
+var path = require('path');
+
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-
-    clean: ['{%= out_path %}/'],
-    copy: {
-      main: {
-        files: [
-          {
-            expand: true,
-            cwd: '{%= assets_path %}/',
-            src: ['**'],
-            dest: '{%= out_path %}/'
-          }
-        ]
-      }
-    },
-    haggerston: {
-      src: '{%= src_path %}/',
-      dest: '{%= out_path %}/' 
-    },
-    watch: {
-      content: {
-        files: [
-          '{%= src_path %}/content/**/*.json',
-          '{%= src_path %}/content/**/*.md',
-          '{%= src_path %}/templates/**/*.html'
-        ],
-        tasks: 'build'
-      }
-    },
-    connect: {
-      site: {
-        options: {
-          base: '{%= out_path %}/'
-        }
-      }
-    }
-
+  // Load config and initialise...
+  require('load-grunt-config')(grunt, {
+    configPath: path.join(__dirname, 'grunt/options'),
+    init: true
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-haggerston');
+  grunt.registerTask('build', ['clean', 'copy:main', 'less', 'haggerston', 'notify:build']);
 
-  grunt.registerTask('build', ['clean', 'copy', 'haggerston']);
   grunt.registerTask('serve', ['build', 'connect', 'watch']);
 
   grunt.registerTask('default', ['build']);
--- template.js
+++ template.js
@@ -2,7 +2,7 @@
  * grunt-init-haggerston
  * https://github.com/haggerstonjs/grunt-init-haggerston
  *
- * Copyright (c) 2013 Kelvin Luck, contributors
+ * Copyright (c) 2014 Kelvin Luck, contributors
  * Licensed under the MIT license.
  *
  * Based on grunt-init-gruntplugin by "Cowboy" Ben Alman
@@ -62,10 +62,13 @@ exports.template = function(grunt, init, done) {
     props.dependencies = {
       'grunt': props.grunt_version,
       'grunt-haggerston': 'git://github.com/haggerstonjs/grunt-haggerston.git',
-      'grunt-contrib-clean': '~0.4.0',
-      'grunt-contrib-copy': '~0.4.0',
-      'grunt-contrib-watch': '~0.2.0',
-      'grunt-contrib-connect': '~0.1.2'
+      'grunt-contrib-clean': '~0.6.0',
+      'grunt-contrib-copy': '~0.5.0',
+      'grunt-contrib-less': '~0.11.4',
+      'grunt-contrib-watch': '~0.6.1',
+      'grunt-contrib-connect': '~0.8.0',
+      'load-grunt-config': '~0.12.0',
+      'grunt-notify': '~0.3.0'
     };
 
     // Files to copy (and process).
@@ -74,10 +77,8 @@ exports.template = function(grunt, init, done) {
     // Add properly-named license files.
     init.addLicenseFiles(files, props.licenses);
 
-    console.log(props);
-
     // Actually copy (and process) files.
-    init.copyAndProcess(files, props);
+    init.copyAndProcess(files, props, {noProcess: 'src/**'});
 
     // Generate package.json file.
     init.writePackageJSON('package.json', props);
